394ad27aaacbf7ac022e0781835f92f1abf2f055
Kelly <125195808+mskellyy@users.noreply.github.com>
2024-11-29 01:38:21 -0700

    more animation

--- src/script.js
+++ src/script.js
@@ -15,3 +15,39 @@ document.addEventListener("DOMContentLoaded", function () {
 
   observer.observe(heroServices);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const packageGrid = document.querySelector(".package-grid");
+
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("animate");
+          observer.unobserve(entry.target); // Stop observing after animation
+        }
+      });
+    },
+    { threshold: 0.1 } // Trigger when 10% of the element is visible
+  );
+
+  observer.observe(packageGrid);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const serviceMenu = document.querySelector(".service-menu");
+
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("animate");
+          observer.unobserve(entry.target); // Stop observing after animation
+        }
+      });
+    },
+    { threshold: 0.1 } // Trigger when 10% of the element is visible
+  );
+
+  observer.observe(serviceMenu);
+});
